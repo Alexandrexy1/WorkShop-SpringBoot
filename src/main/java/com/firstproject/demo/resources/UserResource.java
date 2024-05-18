fdcfd8f38408eb71e434621873a7dd193ec3b0b3
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-05-18 09:24:41 -0300

    User entitiy and User resource

---
+++ src/main/java/com/firstproject/demo/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.firstproject.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstproject.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Alexandre", "Alexandre@test.com", "123456789", "1234");
+		return ResponseEntity.ok().body(user);
+	}
+}
