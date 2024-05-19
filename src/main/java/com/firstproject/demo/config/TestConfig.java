6670baf19d77a3e180960e2e1513d3a26a1b9635
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-05-19 09:21:24 -0300

    UserService, Service annotation, findAll and findById

--- src/main/java/com/firstproject/demo/config/TestConfig.java
+++ src/main/java/com/firstproject/demo/config/TestConfig.java
@@ -10,13 +10,16 @@ import org.springframework.context.annotation.Profile;
 import com.firstproject.demo.entities.User;
 import com.firstproject.demo.repositories.UserRepository;
 
+// takes the "test profile" from application.properties and the "test config" from application-test.properties
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 	
+	// inject dependency JpaRepository (CRUD simply)
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	// Create users in table tb_user
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User(null, "Alexandre", "alexandre@test.com", "122222222", "123456");
---
+++ src/main/java/com/firstproject/demo/services/UserService.java
@@ -0,0 +1,27 @@
+package com.firstproject.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.firstproject.demo.entities.User;
+import com.firstproject.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> optional = userRepository.findById(id);
+		
+		return optional.get();
+	}
+}
